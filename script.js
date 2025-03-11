7e9d453cce4acb12a19974909fdf263f160a9af6
404_YD <147828868+YD404@users.noreply.github.com>
2025-03-11 15:37:45 +0900

    1.8.0

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const movieCards = [
+  // オーバーレイを表示する関数
+  function showOverlay(imgSrc, titleText, captionText) {
+    const overlay = document.getElementById('overlay');
+    const overlayImg = document.getElementById('overlay-img');
+    const overlayTitle = document.getElementById('overlay-title');
+    const overlayCaption = document.getElementById('overlay-caption');
+
+    overlayImg.src = imgSrc;
+    overlayTitle.innerText = titleText;
+    overlayCaption.innerHTML = captionText;  // innerHTMLを使用してHTMLタグを反映
+    overlay.style.display = 'flex';
+  }
+
+  // オーバーレイを閉じる関数
+  function closeOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+  }
+
+  const movieCards = [
+  { img: 'media/movie/movie_004.avif', title: '「Re:BLUE MOMENT」', caption: '東東東監督作品「Re:BLUE MOMENT」<div class="a-button"><a href="https://youtu.be/qJjVPuMcO2g?si=Z2jUE16IYs1cwFX7" class="button" target="_blank">本編映像</a></div><br><br>2025年03月10日' },
   { img: 'media/movie/movie_003.avif', title: '短編映画「ロック・シザース・ペーパーズ」', caption: '仲井飛祐監督作品「ロック・シザース・ペーパーズ」<div class="a-button"><a href="https://youtu.be/7GR_ErCZBLs" class="button" target="_blank">本編映像</a></div><br><br>2024年12月1日' },
   { img: 'media/movie/karuizawa.avif', title: '「軽井沢殺人事件」冒頭背景映像・背景画像', caption: '舞台「軽井沢殺人事件」にて、冒頭に上映する映像及び背景に投影する画像を作成いたしました。<br>また、舞台当日の投影業務も行いました。<br><br>2024年11月18日' },
   { img: 'media/movie/movie_002.avif', title: '【制作中】CGアニメ『トレイン』', caption: 'CGアニメ『トレイン』鋭意制作中' },
@@ -6,8 +26,8 @@ const movieCards = [
 ];
 
 const newsCards = [
+  // { img: 'media/news/movie_005.avif', title:'「超覚人」が『第18回TOHOシネマズ学生映画祭』にノミネート', caption: 'ARGOメンバーも複数人関わった、短編映画作品「超覚人」が、『第18回TOHOシネマズ学生映画祭』にノミネートされました。<br>2025年03月27日、TOHOシネマズ日比谷にて上映されます。<div class="a-button"><a href="https://tyokakujin.wixsite.com/untitled" class="button" target="_blank">詳しく</a></div><br><br>2025年03月08日' },
   { img: 'media/movie/movie_003.avif', title:'「ロック・シザース・ペーパーズ」が『エクストラ部門』を受賞', caption: 'スマホフィルムフェス2024にて「ロック・シザース・ペーパーズ」が『エクストラ部門』を受賞！<div class="a-button"><a href="https://x.com/sumahofilmfes/status/1872422264106594493" class="button" target="_blank">詳しく</a></div><br><br>2024年12月28日' },
-  { img: 'media/noimage.svg', title: '短編映画「今日が人生一日目！」製作決定', caption: '詳細な続報は2024年度内に公開予定！<br><br>2024年12月26日' },
   { img: 'media/noimage.svg', title: '舞台『軽井沢殺人事件』の映像を担当', caption: 'Ｙプロジェクト・舞台製作集団SHIZUKAが企画・製作している『軽井沢殺人事件』にて、映像を担当致します。<div class="a-button"><a href="https://x.com/stage_shizuka" class="button" target="_blank">詳細情報</a></div><br><br>2024年9月21日' },
   { img: 'media/news/news_001.avif', title: 'ARGOホームページ公開', caption: '創作団体ARGOのホームページを公開しました。<br><br>2024年9月21日' },
 ];
@@ -42,24 +62,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // オーバーレイを表示する関数
-  function showOverlay(imgSrc, titleText, captionText) {
-    const overlay = document.getElementById('overlay');
-    const overlayImg = document.getElementById('overlay-img');
-    const overlayTitle = document.getElementById('overlay-title');
-    const overlayCaption = document.getElementById('overlay-caption');
-
-    overlayImg.src = imgSrc;
-    overlayTitle.innerText = titleText;
-    overlayCaption.innerHTML = captionText;  // innerHTMLを使用してHTMLタグを反映
-    overlay.style.display = 'flex';
-  }
-
-  // オーバーレイを閉じる関数
-  function closeOverlay() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.display = 'none';
-  }
 
   // ページに応じたカードの表示
   const path = window.location.pathname;
@@ -131,4 +133,23 @@ function loadMenu() {
 
 document.addEventListener('DOMContentLoaded', () => {
   loadMenu(); // メニューの読み込みを実施
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const historyItems = document.querySelectorAll('.textblock,.card-member-large,.card-member-small');
+
+  historyItems.forEach((item, index) => {
+    item.style.opacity = 0;
+    item.style.transform = 'translateY(20px)'; // 少し下にずらしておく
+
+    const delay = index * 100; // アニメーション開始を少しずつ遅らせる
+      setTimeout(() => {
+          item.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+          item.style.opacity = 1;
+          item.style.transform = 'translateY(0)';
+      }, delay);
+  });
 });
